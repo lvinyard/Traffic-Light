9db71b199d82f256f22110c8aea0078461e32951
lvinyard <lvinyard98@gmail.com>
2018-09-24 16:30:26 -0700

    Finished ui, need to add logic now.

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ echo View the list of source files
 ls -l
 
 mcs -target:library -r:System.Drawing -r:System.Windows.Forms -out:ui.dll ui.cs
-mcs -r:System -r:System.Windows.Forms -r:System.Drawing  -r:ui.dll -out:Assign1.exe driver.cs
+mcs -r:System  -r:System.Windows.Forms -r:System.Drawing  -r:ui.dll -out:Assign1.exe driver.cs
 
 ./Assign1.exe
 
--- ui.cs
+++ ui.cs
@@ -3,12 +3,71 @@
 using System;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Timers;
 public class Program : Form
 {
-
-    //Creating tools
+	//Creating tools
     private Font arial = new Font("Arial", 15, FontStyle.Bold);
     private Pen blackPen = new Pen(Color.Black);
+    
+    
+    //Creating Controls
+    protected void controls()
+    {
+		//Start Button
+		Button startButton = new Button();
+		startButton.Location = new Point(20,780);
+		startButton.Font = new Font("Arial", 10, FontStyle.Bold);
+		startButton.ForeColor = Color.Black;
+		startButton.BackColor = Color.Green;
+		startButton.Text = "Start";
+		Controls.Add(startButton);
+		
+		//GroupBox Rate of Change
+		GroupBox roc = new GroupBox();
+		roc.ForeColor = Color.Black;
+		roc.BackColor = Color.White;
+		roc.Text = "Rate of Change";
+		roc.Location = new Point(130,770);
+		
+		//Making slow RadioButton
+		RadioButton slow = new RadioButton();
+		slow.Location = new Point(25,20);
+		slow.Text = "Slow";
+		roc.Controls.Add(slow);
+
+		//Making medium RadioButton
+		RadioButton medium = new RadioButton();
+		medium.Location = new Point(25,45);
+		medium.Text = "Medium";
+		roc.Controls.Add(medium);
+
+		//Making fast RadioButton
+		RadioButton fast = new RadioButton();
+		fast.Location = new Point(25,70);
+		fast.Text = "Fast";
+		roc.Controls.Add(fast);		
+				
+		Controls.Add(roc);
+		
+		//Stop Button
+		Button stopButton = new Button();
+		stopButton.Location = new Point(20,820);
+		stopButton.Font = new Font("Arial", 10, FontStyle.Bold);
+		stopButton.ForeColor = Color.Black;
+		stopButton.BackColor = Color.Yellow;
+		stopButton.Text = "Stop";
+		Controls.Add(stopButton);
+		
+		//Exit Button
+		Button exitButton = new Button();
+		exitButton.Location = new Point(350,800);
+		exitButton.Font = new Font("Arial", 10, FontStyle.Bold);
+		exitButton.ForeColor = Color.Black;
+		exitButton.BackColor = Color.Red;
+		exitButton.Text = "Exit";
+		Controls.Add(exitButton);
+	}
 
     //
     //Contructor
@@ -17,14 +76,27 @@ public class Program : Form
     {
 
         //Setting Window Properties
-
         Size = new Size(450, 900);
-
         Text = "Assignment 2 - By Lucas Vinyard";
-        BackColor = Color.Gray;
+        BackColor = Color.DarkGreen;
+		FormBorderStyle = FormBorderStyle.FixedSingle;
+		
+        //Call Label for Top of Program
+		initLabel();
+        
+        //Call Controls
+        controls();
+        
+        //Call Clock
+        initClock();
+		
 
-        //Creating Label for Top of Program
-        Label nameLabel = new Label();
+    }//End of Program
+    
+    //Creating Label
+    protected void initLabel()
+    {
+		Label nameLabel = new Label();
         nameLabel.Text = "Traffic Light by Lucas Vinyard";
         nameLabel.BackColor = Color.White;
         nameLabel.AutoSize = false;
@@ -33,9 +105,22 @@ public class Program : Form
         nameLabel.Height = 40;
         nameLabel.Width = 400;
         Controls.Add(nameLabel);
+	}
+	
+    
+    
+    //Creating Clock
+	protected void initClock()
+	{
+	 System.Timers.Timer seiko = new System.Timers.Timer();
+	seiko.Elapsed += new ElapsedEventHandler(seiko_function);
+	}
 
+	//Seiko function
+	protected void seiko_function(System.Object s, ElapsedEventArgs e)
+	{
+	}
 
-    }//End of Program
 
     //On Paint
     protected override void OnPaint(PaintEventArgs e)
@@ -51,7 +136,17 @@ public class Program : Form
         //Draw Rectangle (Black box on bottom and top screen)
         g.FillRectangle(Brushes.White, 0, 750, 450, 910);
         g.FillRectangle(Brushes.White, 0, 0, 450, 40);
-
+        
+		//Drawing Circles
+		SolidBrush grayBrush = new SolidBrush(Color.Gray);
+		
+		RectangleF rect1 = new RectangleF(125,65,200,200);
+		RectangleF rect2 = new RectangleF(125,295,200,200);
+		RectangleF rect3 = new RectangleF(125,525,200,200);
+		g.FillEllipse(grayBrush,rect1);
+		g.FillEllipse(grayBrush,rect2);
+		g.FillEllipse(grayBrush,rect3);
+		
         base.OnPaint(e);
     }// End of OnPaint
 
