430fc186faee3ede21806b7eac498fae87c3b085
lvinyard <lvinyard98@gmail.com>
2018-09-23 22:52:57 -0700

    Test

--- ui.cs
+++ ui.cs
@@ -1,54 +1,59 @@
+//Lucas Vinyard
+
 using System;
 using System.Drawing;
 using System.Windows.Forms;
-public class Program: Form {
-	
-	//Creating tools
-	private Font arial = new Font("Arial", 15, FontStyle.Bold);
-	private Pen blackPen = new Pen(Color.Black);
+public class Program : Form
+{
 
-//
-//Contructor
-//
-	public Program(){
+    //Creating tools
+    private Font arial = new Font("Arial", 15, FontStyle.Bold);
+    private Pen blackPen = new Pen(Color.Black);
 
-	//Setting Window Properties
+    //
+    //Contructor
+    //
+    public Program()
+    {
 
-	Size = new Size(450,900);  
+        //Setting Window Properties
 
-	Text = "Assignment 2 - By Lucas Vinyard";
-	BackColor=Color.Gray;
+        Size = new Size(450, 900);
 
-	//Creating Label for Top of Program
-	 Label nameLabel = new Label();
-		nameLabel.Text = "Traffic Light by Lucas Vinyard";
-		nameLabel.BackColor = Color.White;
+        Text = "Assignment 2 - By Lucas Vinyard";
+        BackColor = Color.Gray;
+
+        //Creating Label for Top of Program
+        Label nameLabel = new Label();
+        nameLabel.Text = "Traffic Light by Lucas Vinyard";
+        nameLabel.BackColor = Color.White;
         nameLabel.AutoSize = false;
-    	nameLabel.TextAlign = ContentAlignment.MiddleCenter;
+        nameLabel.TextAlign = ContentAlignment.MiddleCenter;
         nameLabel.Font = arial;
-		nameLabel.Height = 40;
-		nameLabel.Width = 400;
-		Controls.Add(nameLabel);
+        nameLabel.Height = 40;
+        nameLabel.Width = 400;
+        Controls.Add(nameLabel);
 
 
-	}//End of Program
-	
-//On Paint
-	protected override void OnPaint(PaintEventArgs e) {
-		
-		Graphics g = e.Graphics;
-		
-		//Draw Pen (Line across bottom screen)
-		blackPen.Width = 5;
-		g.DrawLine(blackPen,0,749,450,749);
-		g.DrawLine(blackPen,0,40,450,40);
-		
-		//Draw Rectangle (Black box on bottom and top screen)
-		g.FillRectangle(Brushes.White,0,750,450,910);
-		g.FillRectangle(Brushes.White,0,0,450,40);
-			
-		base.OnPaint(e);
-	}// End of OnPaint
+    }//End of Program
+
+    //On Paint
+    protected override void OnPaint(PaintEventArgs e)
+    {
+
+        Graphics g = e.Graphics;
+
+        //Draw Pen (Line across bottom screen)
+        blackPen.Width = 5;
+        g.DrawLine(blackPen, 0, 749, 450, 749);
+        g.DrawLine(blackPen, 0, 40, 450, 40);
+
+        //Draw Rectangle (Black box on bottom and top screen)
+        g.FillRectangle(Brushes.White, 0, 750, 450, 910);
+        g.FillRectangle(Brushes.White, 0, 0, 450, 40);
+
+        base.OnPaint(e);
+    }// End of OnPaint
 
 
 }// End of Class
