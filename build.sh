13ffb7d4cb721f655d180ca2e7986773136af3a9
lvinyard <lvinyard98@gmail.com>
2018-09-24 21:30:21 -0700

    Finished Clock logic. Needs Rate of chnage still, and alot of Polishing.

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@ rm *.exe
 echo View the list of source files
 ls -l
 
-mcs -target:library -r:System.Drawing -r:System.Windows.Forms -out:ui.dll ui.cs
+mcs -target:library -r:System.Drawing -r:System.Windows.Forms -out:clockLogic.dll clockLogic.cs
+mcs -target:library -r:System.Drawing -r:System.Windows.Forms -r:clockLogic.dll -out:ui.dll ui.cs
 mcs -r:System  -r:System.Windows.Forms -r:System.Drawing  -r:ui.dll -out:Assign1.exe driver.cs
 
 ./Assign1.exe
--- clockLogic.cs
+++ clockLogic.cs
@@ -5,5 +5,52 @@ using System.Timers;
 
 public class clockLogic
 {
-    //See what the counter is at and chnage the color of brush that the circles are used for.
+    public static SolidBrush topcolor(int x)
+    {
+        SolidBrush top;
+        if (x == -1)
+        {
+            top = new SolidBrush(Color.Gray);
+        }
+        else if ((x % 9) >= 0 && (x % 9) < 4)
+        {
+            top = new SolidBrush(Color.Green);
+        }
+        else
+        {
+            top = new SolidBrush(Color.Gray);
+        }
+        return top;
+    }
+
+
+    public static SolidBrush midcolor(int x)
+    {
+        SolidBrush mid;
+
+        if ((x % 9) == 4)
+        {
+            mid = new SolidBrush(Color.Yellow);
+        }
+        else
+        {
+            mid = new SolidBrush(Color.Gray);
+        }
+        return mid;
+    }
+
+    public static SolidBrush botcolor(int x)
+    {
+        SolidBrush bot;
+
+        if ((x % 9) > 4)
+        {
+            bot = new SolidBrush(Color.Red);
+        }
+        else
+        {
+            bot = new SolidBrush(Color.Gray);
+        }
+        return bot;
+    }
 }
